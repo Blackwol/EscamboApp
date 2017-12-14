7160667ec7f9726df4d3abdf6eb6433f70c19d59
Pedro Cardoso <pbac@ecomp.poli.br>
2017-12-14 17:44:25 +0000

    Ajustando a aplicacao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+ # CONFIG PRA ENVIAR EMAIL
+ # config.action_mailer.default_url_options = {host: 'escamboapp.com.br'}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
